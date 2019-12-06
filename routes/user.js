c01b3d3d38ccd76cbfd6543528746f1c1cce2ce1
Piotr Czajkowski <sixtyto@gmail.com>
2019-12-06 21:54:21 +0100

    add token

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
 router.post("/signup", (req, res, next) => {
-  console.log(req.body);
   User.find({ name: req.body.name })
     .exec()
     .then(user => {
@@ -30,13 +30,11 @@ router.post("/signup", (req, res, next) => {
             user
               .save()
               .then(result => {
-                console.log(result);
                 res.status(201).json({
                   message: "User created"
                 });
               })
               .catch(err => {
-                console.log(err);
                 res.status(500).json({
                   error: err
                 });
@@ -46,4 +44,48 @@ router.post("/signup", (req, res, next) => {
       }
     });
 });
+
+router.post("/login", (req, res, next) => {
+  User.find({ name: req.body.name })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "No user"
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Authorisation failed"
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              name: user[0].name,
+              userId: user[0]._id
+            },
+            "KEy",
+            {
+              expiresIn: "1h"
+            }
+          );
+          return res.status(200).json({
+            message: "Authorisation successful",
+            token: token
+          });
+        }
+        res.status(401).json({
+          message: "Authorisation failed"
+        });
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
 module.exports = router;
