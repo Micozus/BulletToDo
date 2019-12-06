23ace3422ce8ae372a99f8913a4ee361ced8ef70
Piotr Czajkowski <sixtyto@gmail.com>
2019-12-06 21:05:00 +0100

    add user fixed

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 const User = require("./models/user");
 
 const userRoutes = require("./routes/user");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
